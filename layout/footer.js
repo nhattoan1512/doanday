024a40b1fb20cf949d298801ea2192d3d1830a33
nhattoan1512 <nguyennhattoan1512@gmail.com>
2022-01-15 16:13:44 +0700

    first commit

---
+++ layout/footer.js
@@ -0,0 +1,102 @@
+let footer = document.querySelector('.footer');
+footer.innerHTML = `
+<div class="footer__container">
+        <div class="grid wide">
+          <div class="row">
+            <div class="col l-4 m-4 c-12 s-12">
+              <div class="footer__container__about">
+                <div class="container__about--logo">
+                  <a href="#">
+                    Travelix
+                  </a>
+                </div>
+                <div class="container__about--text">
+                  <p>
+                  We are committed to providing you with a truly classy and best tour.
+                  </p>
+                </div>
+                <div class="container__about--social">
+                  <ul>
+                    <li>
+                      <i class="fab fa-facebook-square"></i>
+                    </li>
+                    <li>
+                      <i class="fab fa-instagram"></i>
+                    </li>
+                    <li>
+                      <i class="fab fa-twitter"></i>
+                    </li>
+                    <li>
+                      <i class="fab fa-youtube"></i>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col l-4 m-4 c-12 s-12">
+              <div class="footer__container__blog">
+                <h2>Blog Posts</h2>
+                <div class="footer__container__blog--content">
+                  <div class="container__blog__item">
+                    <div class="container__blog__item--img">
+                      <img
+                        src="../assets/images/xfooter_blog_1.jpg.pagespeed.ic.n5JT1wzbm-.webp"
+                        alt=""
+                      />
+                      <div class="container__blog__item--text">
+                        <a href="#">Travel with us this year</a><br />
+                        <span>July 20, 2021</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="container__blog__item">
+                    <div class="container__blog__item--img">
+                      <img
+                        src="../assets/images/xfooter_blog_2.jpg.pagespeed.ic.u_Bp7zMu38.webp"
+                        alt=""
+                      />
+                      <div class="container__blog__item--text">
+                        <a href="#">New destinations for you</a><br />
+                        <span>July 20, 2021</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="container__blog__item">
+                    <div class="container__blog__item--img">
+                      <img
+                        src="../assets/images/xfooter_blog_3.jpg.pagespeed.ic.XDkX_CloU3.webp"
+                        alt=""
+                      />
+                      <div class="container__blog__item--text">
+                        <a href="#">Travel with us this year</a><br />
+                        <span>July 20, 2021</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <div class="col l-4 m-4 c-12 s-12">
+              <div class="footer__container__contact">
+                <h2>Contact Info</h2>
+                <div class="footer__container__contact__content">
+                  <div class="container__contact__info">
+                    <span>Nghi Kim, Vinh, Nghệ An</span>
+                  </div>
+                  <div class="container__contact__info">
+                    <span>+84xxxxxxxxxx</span>
+                  </div>
+                  <div class="container__contact__info">
+                    <span>nguyenxuanmanh2k2@gmail.com</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <span class="copyright">
+            Edit by TEAM_7
+          </span>
+        </div>
+      </div>
+`
